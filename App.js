6e89763c2586e256ae98b91bce1592eeac9deac2
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-12-12 17:33:32 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,45 @@
 var GIPHY_API_URL = "https://api.giphy.com";
-var GIPHY_PUB_KEY = "dc6zaTOxFJmzC";
+var GIPHY_PUB_KEY = "lDDdnHXEBlFLE8KxcJurBa0jXYXKBPya";
 
 App = React.createClass({
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: "",
+            gif: {}
+        };
+    },
+
+
+    handleSearch: function(searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText, function(gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
+    },
+
+    getGif: function(searchingText, callback) {
+        var url = GIPHY_API_URL + "/v1/gifs/random?api_key=" + GIPHY_PUB_KEY + "&tag=" + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif= {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        };
+        xhr.send();
+    },
     render: function() {
         var styles = {
             margin: "0 auto",
@@ -20,43 +58,7 @@ App = React.createClass({
                     />
             </div>
         );
-    },
-    getInitialState() {
-        return {
-            loading: false,
-            searchingText: "",
-            gif: {}
-        };
-    },
-    handleSearch: function(searchingText) {
-        this.setState({
-            loading: true
-        });
-        this.getGif(searchingText, function(gif) {
-            this.setState({
-                loading: false,
-                gif: gif,
-                searchingText: searchingText
-            });
-        }.bind(this));
-    },
-    getGif: function(searchingText, callback) {
-        var url = GIPHY_API_URL + "/v1/gifs/random?api_key=" + GIPHY_PUB_KEY + "&tag=" + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif= {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);
-            }
-        };
-        xhr.send();
     }
-
 });
 
 var app = React.createElement(App);
